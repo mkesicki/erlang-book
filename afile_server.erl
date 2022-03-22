635962b2906abf83237d32e442e059d51fd8ff7f
mkesicki <marc_he_w@poczta.onet.pl>
2022-03-22 21:03:18 +0100

    Add files

---
+++ afile_server.erl
@@ -0,0 +1,21 @@
+-module(afile_server).
+-export([start/1, loop/1]).
+
+
+start(Dir) -> spawn(afile_server, loop, [Dir]).
+
+loop(Dir) ->
+
+    receive
+        {Client, list_dir} ->
+            Client ! {self(), file:list_dir(Dir)};
+        {Client, {get_file, File}} ->
+            Full = filename:join(Dir, File),
+            Client ! {self(), file:read_file(Full)};
+         {Client, {put_file, File, Content}} ->
+            Full = filename:join(Dir, File),
+            {ok, FD} = file:open(Full, [write]),
+            file:write(FD,Content),
+            Client ! {self(), file:read_file(Full)};
+    end,
+    loop(Dir).
