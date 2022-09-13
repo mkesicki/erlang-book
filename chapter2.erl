ae0a5ce2a25da6f378ab374c03829c3b10020563
mkesicki <marc_he_w@poczta.onet.pl>
2022-09-13 13:38:28 +0200

    Add excercises from chapter2

--- chapter2.erl
+++ chapter2.erl
@@ -7,7 +7,3 @@ my_time_func(F) when is_function(F)->
 my_date_string() ->
     {{Year, Month, Day}, {Hour, Minute, Second}} = erlang:localtime(),
     io:format("Current time is: ~4..0w-~2..0w-~2..0w ~2..0w:~2..0w:~2..0w~n", [Year, Month, Day, Hour, Minute, Second]).
-
-
-
-
--- math_functions.erl
+++ math_functions.erl
@@ -1,6 +1,20 @@
 -module(math_functions).
--export([even/1, odd/1]).
+-export([even/1, odd/1, filter/2]).
 
 even(X) -> X rem 2 == 0.
 
 odd(X) -> X rem 2 /= 0.
+
+filter(F, []) -> [];
+filter(F,L) when is_function(F) ->
+
+    [Head | Tail] = L,
+    Check = F(Head),
+
+    if
+        Check == true -> [Head | filter(F, Tail)];
+        true -> filter(F, Tail)
+    end.
+
+ 
+
