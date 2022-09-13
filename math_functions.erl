868dca957c37e95390d08063a8cd5ca0a0b8a079
mkesicki <marc_he_w@poczta.onet.pl>
2022-09-13 16:05:12 +0200

    Add excercises from chapter 2

--- math_functions.erl
+++ math_functions.erl
@@ -1,5 +1,5 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split/1, acc_evens_odds/3, split2/1]).
 
 even(X) -> X rem 2 == 0.
 
@@ -16,5 +16,20 @@ filter(F,L) when is_function(F) ->
         true -> filter(F, Tail)
     end.
 
- 
+split(L) -> acc_evens_odds(L, [], []).
+
+acc_evens_odds([H|T], Odds, Evens) ->
+
+   case (H rem 2) of
+       1 -> acc_evens_odds(T, [H|Odds], Evens);
+       0 -> acc_evens_odds(T, Odds, [H|Evens])
+   end;
+
+acc_evens_odds([], Evens, Odds) -> {Evens, Odds}.
+
+split2(L) ->
+
+    Even = fun(X) -> X rem 2 == 0 end,
+    Odd = fun(X) -> X rem 2 /= 0 end,
+    {filter(Even, L),filter(Odd,L)}.
 
